4f594cfa52d79c309bbaedae01a5ba6aa4a12000
Ismail Ali <ismailchangezi801@gmail.com>
2023-01-13 18:46:10 +0500

    Update Tests

--- src/_test_/cardsCounter.test.js
+++ src/_test_/cardsCounter.test.js
@@ -3,26 +3,25 @@
  * @jest-environment jsdom
  */
 
-import cardsNumber from "../modules/cardsCounter";
+import cardsNumber from '../modules/cardsCounter';
 
-test("counting cards", () => {
-  document.body.innerHTML =
-    '<h1 id="title"></h1>' +
-    '  <div class="PokeCard"></div>' +
-    '  <div class="PokeCard"></div>' +
-    '  <div class="PokeCard"></div>';
+test('counting cards', () => {
+  document.body.innerHTML = '<h1 id="title"></h1>'
+    + '  <div class="PokeCard"></div>'
+    + '  <div class="PokeCard"></div>'
+    + '  <div class="PokeCard"></div>';
 
-  const cardsResult = document.getElementById("title");
+  const cardsResult = document.getElementById('title');
 
-  cardsNumber("PokeCard");
-  expect(cardsResult.textContent).toBe("PokeCodex (3)");
+  cardsNumber('PokeCard');
+  expect(cardsResult.textContent).toBe('PokeCodex (3)');
 });
 
-test("counting cards should be 0", () => {
+test('counting cards should be 0', () => {
   document.body.innerHTML = '<h1 id="title"></h1>';
 
-  const cardsResult = document.getElementById("title");
+  const cardsResult = document.getElementById('title');
 
-  cardsNumber("PokeCard");
-  expect(cardsResult.textContent).toBe("PokeCodex (0)");
+  cardsNumber('PokeCard');
+  expect(cardsResult.textContent).toBe('PokeCodex (0)');
 });
