4592cb068b30dfdc1b0d56605d114dfa05eedcf5
collins kiprotich <collinskiprotich@gmail.com>
2023-01-13 12:52:26 +0300

    Add comments counter function

--- src/modules/commentCounter.js
+++ src/modules/commentCounter.js
@@ -1,4 +1,4 @@
-const commentCounter1 = (classCount, append) => {
+const commentsCount = (classCount, append) => {
   const counter = classCount.length;
   const counterText = document.createElement('p');
   counterText.setAttribute('id', 'counter');
@@ -6,4 +6,4 @@ const commentCounter1 = (classCount, append) => {
   append.appendChild(counterText);
 };
 
-export default commentCounter1;
+export default commentsCount;
